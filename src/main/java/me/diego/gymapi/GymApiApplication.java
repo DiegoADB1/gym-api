9194ca48dbc84ad3330cc2e708556d8af0af13aa
Diego <diegoaragao465@gmail.com>
2023-06-11 16:15:31 -0300

    start project

---
+++ src/main/java/me/diego/gymapi/GymApiApplication.java
@@ -0,0 +1,13 @@
+package me.diego.gymapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymApiApplication.class, args);
+	}
+
+}
